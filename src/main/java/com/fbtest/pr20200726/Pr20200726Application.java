8109a1dae62c69fb7c253d25a0d4cbbf276bcd78
kevinywlai <kevinywlai@gmail.com>
2023-05-25 21:28:20 +0800

    add1

--- src/main/java/com/fbtest/pr20200726/Pr20200726Application.java
+++ src/main/java/com/fbtest/pr20200726/Pr20200726Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pr20200726Application {
 
+	// yw01: add
 	public static void main(String[] args) {
 		SpringApplication.run(Pr20200726Application.class, args);
 	}
